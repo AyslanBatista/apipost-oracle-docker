ba337d64b61da321bde45bda0206c73637ff88f3
ayslan batista <ayslan.batista@hotmail.com>
2022-11-19 00:32:37 -0300

    Initial commit

--- __main__.py
+++ __main__.py
@@ -1,22 +1,27 @@
-from app import main
+from time import sleep
 from datetime import datetime
-import schedule
-import time
+
+from schedule import every
+from schedule import repeat
+from schedule import run_pending
+
+from app import main
 
 # Horario de inicio e Término do programa
 hora_inicial = "07:45"
 hora_final = "23:15"
 
 # Tempo que será executado a função, de 1 em 1 minuto
-schedule.every(1).minutes.do(main)
+@repeat(every(1).minutes)
+def job():
+    if __name__ == "__main__":
+        main()
+
 
 while (
     datetime.today().strftime("%H:%M") > hora_inicial
     and datetime.today().strftime("%H:%M") <= hora_final
 ):
-    if __name__ == "__main__":
-        if main() == True:
-            schedule.run_pending()
-            time.sleep(30)
-        else:
-            time.sleep(30)
+
+    run_pending()
+    sleep(10)
